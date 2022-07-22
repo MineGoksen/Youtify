ae31ee54bce95be00193ffae2b89dd38aaaffc47
sulegulenkeceli <gulensule@gmail.com>
2022-07-22 19:01:35 +0300

    22.07 like eknlentili

--- src/components/CommentPage/CommentPage.js
+++ src/components/CommentPage/CommentPage.js
@@ -1,28 +1,75 @@
-import React, {useState} from 'react';
-import {MusicItem} from "../musicItem/MusicItem";
-import {useParams} from "react-router-dom";
+import React, {useEffect, useState} from 'react';
 import "./CommentPage.css"
-import ReactPlayer from "react-player";
 import {Formik} from "formik";
 import axios from "axios";
-import loginSchema from "../login/LoginValidations";
 
 
 function CommentPage(props) {
     document.body.style.backgroundColor = "gray";
+    const id = localStorage.getItem('id');
+    const song_id = localStorage.getItem('song_id');
+    const [comments,setComments]=useState([]);
+    const [likeNum,setLikeNum]=useState(0);
+    const [liked,setLiked]=useState(1)
+
+    const [commentAdded,setCommentAdded]= useState(false)
+    useEffect(()=>{
+        axios.get('http://127.0.0.1:8000/getComment/'+song_id).then(response => {
+            if (response.status === 200) {
+                var com=[]
+                for (let i = 0; i < response.data.length; i++) {
+                    com[i]=([{comment:response.data[i].Comment_text, user:response.data[i].User_name}])
+                }
+                setComments(com)
+            } else {
+                window.alert(response.data.message)
+                console.log(response.status, response.data.message)
+            }
+        }).catch(error => {
+            console.error('There was an error!', error);
+            window.alert("Liste Oluşturulamadı.")
+        });
+        axios.get('http://127.0.0.1:8000/getLikes/'+song_id).then(response => {
+            if (response.status === 200) {
+               setLikeNum(response.data.like_count)
+            } else {
+                window.alert(response.data.message)
+                console.log(response.status, response.data.message)
+            }
+        }).catch(error => {
+            console.error('There was an error!', error);
+            window.alert("Liste Oluşturulamadı.")
+        });
+    }, [commentAdded])
+
+    function like(){
+        const userData = {user_id: id, song_id: song_id};
+        axios.post('http://127.0.0.1:8000/song_like',userData).then(response => {
+            if (response.status === 200) {
+                setLikeNum(likeNum+liked)
+                setLiked(liked*-1)
+            } else {
+                window.alert(response.data.message)
+                console.log(response.status, response.data.message)
+            }
+        }).catch(error => {
+            console.error('There was an error!', error);
+            window.alert("Liste Oluşturulamadı.")
+        });
+    }
 
     function returnLists() {
-        const id = localStorage.getItem('id');
-        const song_id = localStorage.getItem('song_id');
+
         const formik = {
             initialValues: {
                 comment: ""
             }, onSubmit: values => {
                 if (values.comment !== null) {
-                    const userData = {comment: values.comment, user_id:id, song_id: song_id};
-                    axios.post('http://127.0.0.1:8000/addComment/', userData).then(response => {
+                    const userData = {user_id: id, song_id: song_id};
+                    axios.post('http://127.0.0.1:8000/addComment', userData).then(response => {
                         if (response.status === 200) {
-                            localStorage.setItem('comment', JSON.stringify(response.data.comment))
+                            window.alert(response.data.message)
+                            console.log(response.status, response.data.message)
                         } else {
                             window.alert(response.data.message)
                             console.log(response.status, response.data.message)
@@ -37,14 +84,16 @@ function CommentPage(props) {
 
         return (
             <>
+                <button><img src={require('./review.png')}
+                             alt="my image" width={"32px"} onClick={()=>like()} /></button> : {likeNum}
                 <Formik {...formik} >
                     {formik =>
-                        (<form onSubmit={formik.handleSubmit} id="comment" >
+                        (<form onSubmit={formik.handleSubmit} id="comment">
                             <label htmlFor="comment">Enter Your Comment</label>
                             <input
                                 id="space"
                                 type="text"
-                                name = "comment"
+                                name="comment"
                                 onChange={formik.handleChange}
                                 value={formik.values.comment}/>
                             {formik.errors.comment && formik.touched.comment &&
@@ -53,6 +102,16 @@ function CommentPage(props) {
                             </button>
                         </form>)}
                 </Formik>
+                <div style={{display: "flex"}}>
+                    <div>
+                        {comments.length!==0?  (comments.map((element, index) =>
+                            <div style={{margin: "10px"}}  key={index}>
+                                {element[0].user}
+                                <div className='card'> {element[0].comment}   </div>
+                            </div>)):<div/>
+                        }
+                    </div>
+                </div>
             </>
         )
     }
@@ -61,7 +120,7 @@ function CommentPage(props) {
         <div style={{
             margin: "30px",
             left: '40%'
-        }} >
+        }}>
 
             {returnLists()}
         </div>
