02eb3e3b4769d9649539195ca66486790f07af2d
sulegulenkeceli <sule@jotform.com>
2022-07-29 18:07:55 +0300

    Search page added

--- src/components/musicPage/MusicPage.js
+++ src/components/musicPage/MusicPage.js
@@ -6,7 +6,7 @@ import {Formik} from "formik";
 
 
 function musicPage(props) {
-    const [comments,setComments]=usesdadasdas
+    const [comments,setComments]=useState()
     /*useEffect(() => {
         axios.get('http://127.0.0.1:8000/search/').then(response => {
             if (response.status === 200) {
