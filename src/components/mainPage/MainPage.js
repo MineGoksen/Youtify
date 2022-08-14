9e603f2e29b2a89048ba3c0befecc6ed29ecac93
MineGoksen <minegoksen00@gmail.com>
2022-08-14 14:35:24 +0300

    14.08.2022 user id bug fixed

--- src/components/mainPage/MainPage.js
+++ src/components/mainPage/MainPage.js
@@ -36,7 +36,6 @@ function MainPage(props) {
 
     function returnLists() {
         document.body.style.backgroundColor = "gray";
-
         const formik = {
             initialValues: {
                 list_name: ""
